d781602f8828ac7ab97ad0c8ddb3f9298102558e
sudhakar <nagursudhakar@gmail.com>
2024-02-23 17:19:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask 
+from redis import Redis
+app = Flask(__name__)
+redis_Db = Redis(host=os.getenv('Host'),port=os.getenv('Port'))
+
+@app.route('/')
+def welcomePage():
+    redis_Db.incr('visitorCount')
+    visitorCount = str(redis_Db.get('visitorCount'),'utf-8')
+    return "Welocme to 0 programming! Visitor Count: " +visitorCount
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", debug=True)
+        
+
+
