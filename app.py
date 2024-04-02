63cf3c994966f506552c3eb05373cc3d3fe4ec20
sudhakar <nagursudhakar@gmail.com>
2024-04-02 17:05:46 +0530

    updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask 
+from flask import Flask
 from redis import Redis
 app = Flask(__name__)
 redis_Db = Redis(host=os.getenv('Host'),port=os.getenv('Port'))
@@ -9,8 +9,8 @@ def welcomePage():
     redis_Db.incr('visitorCount')
     visitorCount = str(redis_Db.get('visitorCount'),'utf-8')
     return "Welocme to 0 programming! Visitor Count: " +visitorCount
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", debug=True)
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug=True)
         
 
 
